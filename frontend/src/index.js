cf9b9f274d99e459bba7877ca34459087b78e3ec
DuyVu285 <80942615+DuyVu285@users.noreply.github.com>
2023-05-12 13:31:43 +0700

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,31 +4,30 @@ import App from "./App";
 
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import productsReducer, { productsFetch } from "./features/productsSlice";
 import { productsApi } from "./features/productsApi";
 import cartSlice, { cartReducer } from "./features/cartSlice";
 
-
-
 const store = configureStore({
   reducer: {
     products: productsReducer,
-    cart:cartReducer,
+    cart: cartReducer,
     [productsApi.reducerPath]: productsApi.reducer,
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware().concat(productsApi.middleware);
-  }
+  },
 });
 
-store.dispatch(productsFetch())
+store.dispatch(productsFetch());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Router>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </Router>
 );
